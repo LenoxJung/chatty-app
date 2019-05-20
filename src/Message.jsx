40abeb31736dddb60d11deddc9392bc37ff424ec
Lenox Jung <lenoxjung@gmail.com>
2019-05-20 23:04:56 +0000

    changed regex to match localhost img urls

--- src/Message.jsx
+++ src/Message.jsx
@@ -7,11 +7,11 @@ class Message extends Component {
   }
 
   render() {
-    let messages = this.props.content.split(/(https?:[/|.|\w|\s|-]*\.(?:jpg|gif|png))/gi);
+    let messages = this.props.content.split(/(https?:[/|.|\w|\s|-|:]*\.(?:jpg|gif|png))/gi);
     messages = messages.filter(message => !message == "");
     const content = messages.map((message, index) => {
       return (
-        <div key={index}>{/(https?:[/|.|\w|\s|-]*\.(?:jpg|gif|png))/gi.test(message) ? (<img src={message} />) : (message)}<br/></div>
+        <div key={index}>{/(https?:[/|.|\w|\s|-|:]*\.(?:jpg|gif|png))/gi.test(message) ? (<img src={message} />) : (message)}<br/></div>
       )
     });
     return this.props.type == "incomingMessage"? (
